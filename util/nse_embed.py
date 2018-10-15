2143ec6d0aae9db1f28a4eacf15730ba64cbb866
Bryce Schroeder <bryce@gnosticinstruments.com>
2018-10-14 23:18:02 -0400

    Modified nse_embed, added test data.

--- util/nse_embed.py
+++ util/nse_embed.py
@@ -17,62 +17,131 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import sys, os, base64
+import sys, os, base64, json
+mode = sys.argv[1] if len(sys.argv) > 2 else 'none'
+
+def srcatr(mime, data_or_path):
+  if mode == 'embed':
+    data_or_path = str(base64.b64encode(data_or_path), 'utf-8')
+    return "data:%s;base64,"%mime + data_or_path
+  else:
+    return data_or_path
 
 def img_tag(mime, name, data):
-  return '<img id="%s" style="visibility:hidden" src="data:%s;base64,%s">\n'%(
-    name, mime, str(base64.b64encode(data), 'utf-8')
-    )
+  return '<img id="%s" style="display:none" src="%s">\n'%(
+    name, srcatr(mime, data))
 
-def txt_tag(name, data):
-  return '<div id="%s" style="visibility:hidden">%s</div>\n'%(
-    name, str(data, 'utf-8').strip())
+def txt_tag(mime, name, data):
+  global loaders
+  if mode == 'embed':
+    return '<div id="%s" style="display:none">%s</div>\n'%(
+      name, str(data, 'utf-8'))
+  else:
+    loaders.append("_nse_object_loader('_nse_%s', '%s');"%(name,name))
+    return '''<object id="_nse_%s" type="%s" data="%s" style="display:none"
+ onload="_nse_object_loader('_nse_%s', '%s')">
+      </object>\n'''%(
+      name, mime, data,name,name)
+
+def json_tag(name, data):
+  global loaders
+  if mode == 'embed':
+    data = json.loads(str(data, 'utf-8'))
+    return '''<script>\nnse_res["%s"] = %s;\n</script>\n'''%(
+      name, json.dumps(data))
+  else:
+    return '''<object id="_nse_%s" type="application/json" data="%s" style="display:none"
+ onload="_nse_json_loader('_nse_%s', '%s')">
+      </object>\n'''%(
+      name, data,name,name)
+
 
 def audio_tag(mime, name, data):
-  return '''<audio id="%s" preload="auto" style="visibility:hidden">
-      <source src="data:%s;base64,%s" />
-      </audio>\n'''%(
-    name, mime, str(base64.b64encode(data), 'utf-8')
+  if mode == 'embed':
+    return '''<audio id="%s" preload="auto" style="display:none">
+  <source src="%s" />
+</audio>\n'''%(
+    name, srcatr(mime, data)
     )
+  else:
+    return '''<audio id="%s" preload="auto" style="display:none">
+  <source src="%s"/></audio>\n'''%(name, data)
 def css_tag(name, data):
-  return '''<style id="%s" onload="this.disabled=true">\n%s</style>\n'''%(
-    name, str(data, 'utf-8')
-    )
+  if mode == 'embed':
+    return '''<style id="%s" onload="this.disabled=true">\n%s</style>\n'''%(
+      name, str(data, 'utf-8'),
+      )
+  else:
+    return '<style id="%s" onload="this.disabled=true" src="%s"></style>\n'%(
+      name, data)
 def script_tag(name, data):
-  return '''<script id="%s">\n%s\n</script>\n'''%(
-    name, str(data, 'utf-8')
-    )
+  if mode == 'embed':
+    return '''<script id="%s">\n%s\n</script>\n'''%(
+      name, str(data, 'utf-8'))
+  else:
+    return '<script id="%s" src="%s">\n'%(name, data)
 
 HANDLERS = {
   'jpg': (lambda fn,d: img_tag('image/jpeg', fn, d)),
   'png': (lambda fn,d: img_tag('image/png', fn, d)),
   'gif': (lambda fn,d: img_tag('image/gif', fn, d)),
   'svg': (lambda fn,d: img_tag('image/svg+xml', fn, d)),
-  'txt': (lambda fn,d: txt_tag(fn, d)),
-  'html':(lambda fn,d: txt_tag(fn, d)),
-  'htm': (lambda fn,d: txt_tag(fn, d)),
+  'txt': (lambda fn,d: txt_tag('text/plain', fn, d)),
+  'html':(lambda fn,d: txt_tag('text/html', fn, d)),
+  'htm': (lambda fn,d: txt_tag('text/html', d)),
   'css': (lambda fn,d: css_tag(fn, d)),
   'js': (lambda fn,d: script_tag(fn, d)),
+#  'json': (lambda fn,d: json_tag(fn, d)),
   'ogg': (lambda fn,d: audio_tag('audio/ogg', fn, d)),
   'wav': (lambda fn,d: audio_tag('audio/wav', fn, d)),
   'mp3': (lambda fn,d: audio_tag('audio/mpeg', fn, d)),
 }
 
 PREFIX = """
-<!-- Embedded Resources Begin -->
+<!-- Included Resources Begin -->
+
+<div style="display:none" id="_nse_loaded_objects"></div>
+
+<script>
+"use strict;"
+
+let nse_res = {};
+
+function _nse_json_loader(object_id, symbolic_name) {
+  console.info("_nse_json_loader");
+}
+
+function _nse_object_loader(object_id, new_div_id) {
+  //console.info("_nse_object_loader");
+  //console.info(object_id);
+  //console.info(new_div_id);
+  let object_contents = document.getElementById(
+    object_id).contentDocument.getElementsByTagName("body")[0].innerHTML;
+  //console.info(object_contents);
+  let destination = document.getElementById("_nse_loaded_objects");
+  let new_div = document.createElement("div");
+  new_div.setAttribute("id", new_div_id);
+  new_div.innerHTML = object_contents;
+  destination.appendChild(new_div);
+}
+</script>
+
 """
 SUFFIX = """
-<!-- Embedded Resources End -->
+<!-- Included Resources End -->
 """
 
+TAG = "<!-- _NSE_RESOURCES -->"
 
-if len(sys.argv) < 3:
+
+if len(sys.argv) < 4 or mode not in ('link','embed'):
   print("""
-nse-embed - Assemble image, text/html, css, js and sound resources into a 
-single HTML page for distribution.
+nse_embed - Assemble image, text/html, css, js and sound resources into a 
+single HTML page for distribution. The comment <!-- _NSE_RESOURCES --> will be
+replaced by the resources.
 
-Usage: nse-embed outfile.html one/ or/ more/ directories/ or files to include
-e.g. nse-embed pcres.html sprites/ sounds/ text/ music.ogg graphics.png
+Usage: nse_embed embed outfile.html one/ or/ more/ directories/
+e.g. nse_embed embed pcres.html sprites/ sounds/ text/ music.ogg graphics.png
 
 Naming conventions: 
 Images become <img> tags with an id corresponding to their directory
@@ -103,16 +172,24 @@ The script tries to detect this and issue a warning.
   sys.exit(-1)
 
 seen_ids = {}
+loaders = []
 
-output_file = open(sys.argv[1], 'w')
+template_start, template_end = open(
+  sys.argv[2], 'r').read().split(TAG)
+
+
+
+
+output_file = open(sys.argv[2].replace('.html', '.pack.html'), 'w')
+print (template_start, file = output_file)
 print (PREFIX, file = output_file)
 
-directories = sys.argv[2:]
+directories = sys.argv[3:]
 
 
 for directory in directories:
   for root, dirs, files in os.walk(directory):
-    path_prefix = '_'.join([x for x in os.path.split(root) if x])
+    path_prefix = '_'.join([x for x in root.split(os.path.sep) if x])
     for filen in files:
       basename,typeext = os.path.splitext(filen)
       typeext = typeext.lower()[1:]
@@ -120,15 +197,22 @@ for directory in directories:
       if typeext not in HANDLERS:
         print ("Ignoring unknown type of file: `%s'"%filen, file=sys.stderr)
       else:
-        data = open(os.path.join(root,filen), 'rb')
+        datapath = os.path.join(root,filen)
+        data = open(datapath, 'rb') if mode == 'embed' else None
         idnm = path_prefix + '_' + basename
         if idnm in seen_ids:
           print ("Warning: duplicate ID: ", idnm, file=sys.stderr)
         seen_ids[idnm] = True
-        tag = HANDLERS[typeext](idnm, data.read())
-        data.close()
+        tag = HANDLERS[typeext](
+          idnm, data.read() if mode == 'embed' else datapath)
+        if mode == 'embed': data.close()
         output_file.write(tag)
 
-
+#if mode == 'link':
+#  print ("<!-- Loaders for linked objects -->\n<script>", file=output_file)
+#  for loader in loaders:
+#    print (loader, file=output_file)
+#  print ("</script>", file=output_file)
 print (SUFFIX, file = output_file)
+print (template_end, file = output_file)
 output_file.close()
